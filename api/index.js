d42973dafb8aecf1ecfd366c7b8822e9c5adfde0
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-04-12 22:14:23 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('mongoDB is connected');
+}
+).catch((err) => console.log('Error->' +err));
 
 const app = express();
-
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
