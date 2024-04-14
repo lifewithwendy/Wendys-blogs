5af6dbf81958058f8006114cfc31f05722cd90b9
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-04-14 22:40:39 +0530

    add functionality to Signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,50 @@
-import { Button, Label } from 'flowbite-react';
+import {TextInput, Button, Label, Alert, Spinner } from 'flowbite-react';
+import { set } from 'mongoose';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [loading, setloading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim()});
+  };
+  const handleSubmission = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      setErrorMessage('Please fill in all fields');
+      return;
+    }
+    console.log(formData);
+
+    try {
+      setloading(true);
+      setErrorMessage(null);
+
+      const res= await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if(data.success === false){
+        return setErrorMessage(data.message);
+      }
+      setloading(false);
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setloading(false);
+    }
+  }
+
   return (
     <div className='min-h-screen mt-20'>
       <div className="flex p-3 max-w-3xl mx-auto flex-col  md:flex-row  md:items-center gap-5">
@@ -23,49 +65,68 @@ export default function SignUp() {
         {/* Right side */}
         <div className="flex-1">
           <div className="">
-            <form className='flex flex-col gap-4'>
+            <form className='flex flex-col gap-4' onSubmit={handleSubmission}>
 
               <div className="">
                 <Label className="text-sm">Your Username</Label>
-                <input 
+                <TextInput 
                   type="text" 
-                  className="w-full p-2 border border-gray-300 rounded-md" 
+                  className="w-full p-2 border-0 border-gray-300 rounded-md" 
                   placeholder="Username"
                   id='username'
+                  onChange={handleChange}
                 />
               </div>
 
               <div className="">
               <Label className="text-sm">Your Email</Label>
-                <input 
-                  type="text" 
-                  className="w-full p-2 border border-gray-300 rounded-md" 
+                <TextInput 
+                  type="email" 
+                  className="w-full p-2 border-0 border-gray-300 rounded-md" 
                   placeholder="Name@company.com"
-                  id='username'
+                  id='email'
+                  onChange={handleChange}
                 />
               </div>
 
               <div className="">
               <Label className="text-sm">Your password</Label>
-                <input 
-                  type="text" 
-                  className="w-full p-2 border border-gray-300 rounded-md" 
+                <TextInput 
+                  type="password" 
+                  className="w-full p-2 border-0 border-gray-300 rounded-md" 
                   placeholder="Password"
-                  id='username'
+                  id='password'
+                  onChange={handleChange}
                 />
               </div>
 
-              <Button gradientDuoTone='purpleToPink' type='submit' className=''>
-                Sign Up
+              <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+                
+                {
+                  loading ? (
+                    <>
+                    <Spinner size='sm' />
+                    <span className="pl-3">Loading...</span>
+                    </>
+                ) : (
+                  'Sign Up'
+                )}
               </Button>
 
             </form>
-            <div className="flex gap-2 text-sm ">
+            <div className="flex gap-2 text-sm mt-2">
               <span className="text-sm">Already have an account?</span>
               <Link to='/sign-in' className='text-blue-500'> 
               Sign In
               </Link>
             </div>
+            {
+              errorMessage && (
+                <Alert className="mt-2" color='failure'>
+                  {errorMessage}
+                </Alert>
+              )
+            }
           </div>
         </div>
       </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {//configuring a proxy for the server to connect front end to backend
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
