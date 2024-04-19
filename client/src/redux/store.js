2f8fb014e62ff63adaa307fa5f472b6781708e0e
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-04-20 02:05:11 +0530

    redux persist configuration

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+//creating local storage to keep track on states
+const rootReducer = combineReducers({//merge all reducers
+  user: userReducer,
+});
+
+const persistConfig = {//config os persist
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => //without this error will happen
+  getDefaultMiddleware({serializableCheck: false }),
 })
+
+
+export const persistor = persistStore(store);
