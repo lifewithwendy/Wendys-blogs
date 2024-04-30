343613d3de1f5660d9f6c7e7c56774a86fc768d4
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-04-30 17:14:52 +0530

    update profile update functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();//importing critical things which are stored in remote files for security
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO)//database connectivity
 const app = express();//using express to connect
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
