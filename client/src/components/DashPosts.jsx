74260f3a5e85cfe7dd82bff0e39aa7c980fbe230
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-05-07 20:21:54 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -2,10 +2,12 @@ import React,{ useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Table } from 'flowbite-react'
 import { Link } from 'react-router-dom'
+import { set } from 'mongoose';
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,7 +16,9 @@ export default function DashPosts() {
         const data = await res.json();
         if(res.ok){
           setUserPosts(data.posts);
-          
+          if(data.posts.length > 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error);
@@ -24,13 +28,26 @@ export default function DashPosts() {
       fetchPosts(); 
     }
   	}, [currentUser._id])
+    const handleShowMore = async () => {
+      const startIndex = userPosts.length;
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+        const data = await res.json();
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
+        setUserPosts([...userPosts, ...data.posts]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   return (
-    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 
+    <div className='table-auto w-full overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 
     scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {
         currentUser.isAdmin && userPosts.length > 0 ? (
           <>
-            <Table hoverable className='shadow-md'>
+            <Table hoverable className='shadow-md '>
               <Table.Head>
                 <Table.HeadCell>Date updated</Table.HeadCell>
                 <Table.HeadCell>Post Image</Table.HeadCell>
@@ -84,7 +101,13 @@ export default function DashPosts() {
                 ))
               }
 
-            </Table>
+            </Table>{
+              showMore && (
+                <button onClick={ handleShowMore }className='w-full text-teal-500 self-center text-sm py-7'>
+                  Show More
+                </button>
+              )
+            }
           </>
         ) : (
           <p>You have no Posts yet!</p>
