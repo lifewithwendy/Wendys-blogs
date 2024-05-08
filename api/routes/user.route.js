2a98ecf356e8d3ec9ec66db6e2b00c10eed4078d
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-05-08 12:51:34 +0530

    create getUsers api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signout } from '../controllers/user.controller.js';
+import { test, updateUser, deleteUser, signout, getUsers } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+import { get } from 'mongoose';
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.get('/test',test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId',verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
