f4745b528bc21be59b173aac5ec5d77822617889
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-05-10 00:45:32 +0530

    show to comments of the post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,19 +1,20 @@
 import { Alert, Button,Textarea } from 'flowbite-react';
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Comment from './Comment';
 
 
 export default function CommentSection({postId}) {
     const { currentUser } = useSelector(state => state.user);
-    const [comments, setComments] = useState('');
+    const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(false);
-
+    const [comments, setComments] = useState([]);
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log('submitting comment');
-        e.preventDefault();
-        if( comments.length > 200 ){
+        if( comment.length > 200 ){
             return;
         }
         try{
@@ -23,15 +24,16 @@ export default function CommentSection({postId}) {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    content: comments,
+                    content: comment,
                     postId,
                     userId: currentUser._id
                 }),
             });
             const data = await res.json();
             if (res.ok){
-                setComments('');
+                setComment('');
                 setCommentError(null);
+                setComments([data, ...comments])
             }
         }catch(error){
             console.log(error.message);
@@ -39,6 +41,23 @@ export default function CommentSection({postId}) {
         }
 
     }
+
+    useEffect(() => {
+        const getComments = async () => {
+            try {
+                const res = await fetch(`/api/comment/getPostComments/${postId}`);
+                if(res.ok){
+                    const data= await res.json();
+                    setComments(data);
+                    console.log(comments)
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        getComments();
+    },[postId])
+
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {
@@ -69,11 +88,11 @@ export default function CommentSection({postId}) {
                     placeholder='Add a comment' 
                     rows='3'
                     maxLength='200'
-                    onChange={(e) => setComments(e.target.value)}
-                    value={comments}	
+                    onChange={(e) => setComment(e.target.value)}
+                    value={comment}	
                 />
                 <div className="flex justify-between items-center">
-                    <p className='text-gray-500'>{200-comments.length} characters remaining</p>
+                    <p className='text-gray-500'>{200-comment.length} characters remaining</p>
                     <Button 
                         outline 
                         gradientDuoTone='greenToBlue'
@@ -87,8 +106,24 @@ export default function CommentSection({postId}) {
                     </Alert>
                 }
             </form>
-        )
-      }
+        )}
+            {comments.length === 0 ? (
+                <p className='text-sm my-5'>No comments yet!</p>
+            ) : (
+                <>
+                    <div className="flex flex-row text-sm my-5 items-center gap-1">
+                        comments
+                        <div className="border border-gray-400 py-1 px-2 rounded-sm">
+                            <p>{comments.length}</p>
+                        </div>
+                    </div>
+                    {
+                        comments.map((comment) =>(
+                            <Comment key={comment._id} comment={comment}/>
+                        ))
+                    }
+                </>
+            )}
     </div>
   )
 }
