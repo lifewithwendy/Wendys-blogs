ae20f8f4f9b19f1d8d4ec1026c807d03eeec057b
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-05-14 00:02:38 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex item-center justify center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center">About Wendy's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p className="">
+              Wendy's Blog is a full-stack web application that allows users to create, read, update, and delete blog posts. The frontend is built with React.js and Tailwind CSS, and the backend is built with Node.js, Express.js, and MongoDB.
+            </p>
+            <p className="">
+              Wendy's Blog is a full-stack web application that allows users to create, read, update, and delete blog posts. The frontend is built with React.js and Tailwind CSS, and the backend is built with Node.js, Express.js, and MongoDB.
+            </p>
+            <p className="">
+              Wendy's Blog is a full-stack web application that allows users to create, read, update, and delete blog posts. The frontend is built with React.js and Tailwind CSS, and the backend is built with Node.js, Express.js, and MongoDB.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 export default function Projects() {
   return (
-    <div>
-      Projects
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center item-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>
+        Wendy's Blog is a full-stack web application that allows users to create, read, update, and delete blog posts. The frontend is built with React.js and Tailwind CSS, and the backend is built with Node.js, Express.js, and MongoDB.
+      </p>
+      <CallToAction />
     </div>
   )
 }
