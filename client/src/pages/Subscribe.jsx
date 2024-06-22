698be5011c517743063fda0922fc4cddf7c06177
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-06-22 11:23:08 +0530

    create Subscription UI sub part

--- client/src/pages/Subscribe.jsx
+++ client/src/pages/Subscribe.jsx
@@ -1,44 +1,111 @@
 import { Button } from 'flowbite-react'
 import React,{ useEffect, useState, } from 'react'
 import { useSelector } from 'react-redux'
+import { FaCheck, FaTimes } from 'react-icons/fa';
+
 
 
 export default function Subscribe() {
   const { currentUser } = useSelector(state => state.user);
-  useEffect(() => {
-    const getsubs = async () => {
-      const res = await fetch('/api/sub/makepayment', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          userId : currentUser._id,        
-          type: '1',
-        }),
-      });
-      const data = await res.json();
-      if(!res.ok) {
-        console.log(res.error)
-      }
-      if(res.ok) {
-        console.log(data)
-        // navigate(`/home`);
-      }
+  const [subType, setSubType] = useState(null);
+  const Items = [
+    {
+      type:1,
+      name:'Weekly plan',
+      price: "500LKR",
+      benefits: [
+        'benefits 1',
+        'benefits 2',
+        'benefits 3',
+        'benefits 4'
+      ]
+    },{
+      type:2,
+      name:'Monthly plan',
+      price: "1500LKR",
+      benefits: [
+        'benefits 1',
+        'benefits 2',
+        'benefits 3',
+        'benefits 4'
+      ]
+    },{
+      type:3,
+      name:'Annual plan',
+      price: "10000LKR",
+      benefits: [
+        'benefits 1',
+        'benefits 2',
+        'benefits 3',
+        'benefits 4'
+      ]
     }
-    try {
-      getsubs();
-  } catch (error) {
-      console.log(error);
-  }
-  },[])
+  ];
+  console.log(subType)
+  // useEffect(() => {
+  //   const getsubs = async () => {
+  //     const res = await fetch('/api/sub/makepayment', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         userId : currentUser._id,        
+  //         type: '1',
+  //       }),
+  //     });
+  //     const data = await res.json();
+  //     if(!res.ok) {
+  //       console.log(res.error)
+  //     }
+  //     if(res.ok) {
+  //       console.log(data)
+  //       // navigate(`/home`);
+  //     }
+  //   }
+  //   try {
+  //     getsubs();
+  // } catch (error) {
+  //     console.log(error);
+  // }
+  // },[])
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <Button 
-        className=''
-        >
-        Subscribe
-      </Button>
+    <div className="">
+      {!subType ? (
+        <div className="min-h-screen flex flex-col items-center">
+        <h1 className="text-5xl text-center font-bold mt-14">Pick your <span className='text-blue-800'>perfect</span> plan</h1>
+        <h1 className="text-2xl font-bold mt-6">14 days free trial</h1>
+        <h1 className="text-1xl text-center text-gray-400 ">Get the right plan for you. Plans can be upgraded in the future.</h1>
+        <div className="flex flex-col mb-10 mt-[-18px] sm:my-6 mx-10 sm:flex-row justify-center items-center w-[95%]  gap-5 ">
+          {Items.map((item) => (
+            <div
+              key={item.type}
+              className="flex flex-col p-3 shadow-[3px_5px_5px_1px_rgba(0,0,0,0.2)] w-[350px] h-[450px] mt-12 border-solid gap-4 border-[1px] rounded-[30px] border-gray-400 justify-between items-center"
+            >
+              <h1 className='text-[20px] mt-3 font-bold'>{item.name}</h1>
+              <h1 className='text-5xl font-bold sm:text-1xl'>{item.price}</h1>
+              <ul className=''>
+                {item.benefits.map((benefit) => (
+                  <li className="flex flex-row  gap-3 mt-2" key={benefit}><FaCheck className='text-blue-800 mt-1 mr-3 '/>{benefit}</li>
+                ))}
+              </ul>
+              <button
+                onClick={() => {
+                  setSubType(item);
+                }}
+                className="w-[23vh] mb-3 hover:bg-blue-800 hover:text-white border-solid font-semibold text-blue-800 border-2 p-2 rounded-[20px] border-blue-800 "
+              >
+                Choose Plan
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      ):(
+        <>
+        </>
+      )}
+      
     </div>
-  )
+  );
 }
